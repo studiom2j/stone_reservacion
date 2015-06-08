55aa372e66a9156aed0020361d1391cff0fc2b0e
Jesus Gutierrez <jesusdavid1004@gmail.com>
2015-06-08 16:13:44 -0430

    Cambio model cliente

--- models/Cliente.php
+++ models/Cliente.php
@@ -11,14 +11,16 @@ use Yii;
  * @property string $nombre_cliente
  * @property string $apellido_cliente
  * @property string $cedula_cliente
+ * @property string $fecha_nacimiento_cliente
  * @property string $sexo_cliente
  * @property string $email_cliente
  * @property string $direccion_cliente
- * @property string $fecha_nacimiento_cliente
+ * @property string $nacionalidad_cliente
+ * @property string $telefono_movil_cliente
+ * @property string $telefono_fijo_cliente
  * @property string $facebook_cliente
  * @property string $twitter_cliente
  * @property string $website_cliente
- * @property string $nacionalidad_cliente
  *
  * @property Usuario[] $usuarios
  */
@@ -38,13 +40,14 @@ class Cliente extends \yii\db\ActiveRecord
     public function rules()
     {
         return [
-            [['nombre_cliente', 'apellido_cliente', 'cedula_cliente', 'sexo_cliente', 'email_cliente', 'direccion_cliente', 'fecha_nacimiento_cliente', 'nacionalidad_cliente'], 'required'],
-            [['direccion_cliente'], 'string'],
+            [['nombre_cliente', 'apellido_cliente', 'cedula_cliente', 'fecha_nacimiento_cliente', 'sexo_cliente', 'email_cliente', 'direccion_cliente', 'nacionalidad_cliente', 'telefono_movil_cliente'], 'required'],
             [['fecha_nacimiento_cliente'], 'safe'],
+            [['direccion_cliente'], 'string'],
             [['nombre_cliente', 'apellido_cliente'], 'string', 'max' => 60],
             [['cedula_cliente'], 'string', 'max' => 10],
             [['sexo_cliente'], 'string', 'max' => 20],
-            [['email_cliente', 'facebook_cliente', 'twitter_cliente', 'website_cliente', 'nacionalidad_cliente'], 'string', 'max' => 45],
+            [['email_cliente', 'nacionalidad_cliente', 'facebook_cliente', 'twitter_cliente', 'website_cliente'], 'string', 'max' => 45],
+            [['telefono_movil_cliente', 'telefono_fijo_cliente'], 'string', 'max' => 12],
             [['cedula_cliente'], 'unique']
         ];
     }
@@ -59,14 +62,16 @@ class Cliente extends \yii\db\ActiveRecord
             'nombre_cliente' => 'Nombre Cliente',
             'apellido_cliente' => 'Apellido Cliente',
             'cedula_cliente' => 'Cedula Cliente',
+            'fecha_nacimiento_cliente' => 'Fecha Nacimiento Cliente',
             'sexo_cliente' => 'Sexo Cliente',
             'email_cliente' => 'Email Cliente',
             'direccion_cliente' => 'Direccion Cliente',
-            'fecha_nacimiento_cliente' => 'Fecha Nacimiento Cliente',
+            'nacionalidad_cliente' => 'Nacionalidad Cliente',
+            'telefono_movil_cliente' => 'Telefono Movil Cliente',
+            'telefono_fijo_cliente' => 'Telefono Fijo Cliente',
             'facebook_cliente' => 'Facebook Cliente',
             'twitter_cliente' => 'Twitter Cliente',
             'website_cliente' => 'Website Cliente',
-            'nacionalidad_cliente' => 'Nacionalidad Cliente',
         ];
     }
 
